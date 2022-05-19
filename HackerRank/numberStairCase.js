2cbb804c90efad595e7ffb6b3988b1f6e6503f7e
alexissorianooo <alexis.soriano27@gmail.com>
2022-05-19 18:41:09 +0800

    update

---
+++ HackerRank/numberStairCase.js
@@ -0,0 +1,14 @@
+function staircase(n) {
+    // Write your code here
+    let space=n-1
+    let hashtag=1
+    while(n>0){
+        console.log(`${" ".repeat(space)}${"#".repeat(hashtag)}`)
+        // console.log(space,hashtag)
+        hashtag++
+        space--
+        n--
+    }
+}
+
+staircase(4)
